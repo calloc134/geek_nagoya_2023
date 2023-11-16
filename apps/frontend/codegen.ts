6b2e2119cd3d287835d76508834553df3eed048e
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-11-16 14:08:31 +0000

    chore: prepareにgraphql codegenを追加

---
+++ apps/frontend/codegen.ts
@@ -0,0 +1,24 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "../../schema.graphql",
+  documents: ["./src/components/**/*.tsx"],
+  ignoreNoDocuments: true,
+  generates: {
+    "src/lib/generated/": {
+      preset: "client",
+      config: {
+        strictScalars: true,
+        scalars: {
+          File: "File",
+        },
+        enumsAsTypes: true,
+        skipTypename: true,
+        useTypeImports: true,
+      },
+    },
+  },
+};
+
+export default config;
