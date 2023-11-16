a42957a6f751710b5a3322078c5eb6b9549cd160
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-11-16 14:05:02 +0000

    feat: graphqlのバックエンド最低限実装

---
+++ apps/backend/codegen.ts
@@ -0,0 +1,23 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "../../schema.graphql",
+  generates: {
+    "src/lib/generated/resolver-types.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        contextType: "../../context#GraphQLContext",
+        strictScalars: true,
+        scalars: {
+          File: "File",
+        },
+        enumsAsTypes: true,
+        skipTypename: true,
+        useTypeImports: true,
+      },
+    },
+  },
+};
+
+export default config;
--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -1,14 +1,19 @@
 import { createSchema } from "graphql-yoga";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { QueryResolvers } from "./lib/generated/resolver-types";
+
+const query_resolvers: QueryResolvers = {
+  hello: async (_parent, _args, _context, _info) => {
+    return {
+      message: "Hello World!",
+      url: "https://google.com",
+    };
+  },
+};
 
 export const schema = createSchema({
-  typeDefs: /* GraphQL */ `
-    type Query {
-      hello: String
-    }
-  `,
+  typeDefs: [loadFilesSync("../../schema.graphql")],
   resolvers: {
-    Query: {
-      hello: () => "world",
-    },
+    Query: query_resolvers,
   },
 });
