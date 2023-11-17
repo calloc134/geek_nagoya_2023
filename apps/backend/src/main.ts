cf7ba4fc8f3f723da056b187ab4602a057f43a8e
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-11-17 08:10:46 +0000

    fix: corsを定義

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,15 @@ import { createServer } from "node:http";
 import { schema } from "./schema";
 
 // Create a Yoga instance with a GraphQL schema.
-const yoga = createYoga({ schema });
+const yoga = createYoga({
+  schema,
+  cors: {
+    origin: "https://geek-nagoya-2023.pages.dev",
+    credentials: true,
+    methods: ["POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+});
 
 // Pass it into a server to hook into request handlers.
 const server = createServer(yoga);
